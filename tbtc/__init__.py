597a51620b73125196d4eca1c5a2dbafcb5c01dd
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2020-10-30 13:32:26 +0000

    implemented electrum and updated readme

--- tbtc/__init__.py
+++ tbtc/__init__.py
@@ -1,5 +1,6 @@
 from tbtc.tbtc_system import TBTC
 from tbtc.deposit import Deposit
+from tbtc.electrum import Electrum
 from tbtc.constants import contracts
 from tbtc.session import setup_logging
 
